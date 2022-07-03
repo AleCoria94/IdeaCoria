5a90a0d39c9f0f05b0bb221c814b981e737f6065
Alexis Coria <coria.alexis94@gmail.com>
2022-07-02 21:31:51 -0300

    maps_promises

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,38 @@
 import React from 'react';
 
-function ItemListContainer({titulo}){
-return <h1>{titulo}</h1>
+import { productsData } from "../data/productsData";
+import { useEffect, useState} from "react";
+import ItemList from "./ItemList";
+
+function ItemDetailContainer (){
+    const[products, setProducts]= useState([])
+    const[loading, setLoading] = useState(true)
+
+useEffect (() => {
+getProducts().then( data => {
+    setProducts(data);
+})
+})
+
+const getProducts = ()=>{
+    return new Promise ((resolve, eject) =>{
+        setTimeout (()=>{
+            setLoading(false)
+            resolve(productsData)
+        },2000);
+    })
 }
-export default ItemListContainer;
+
+    return (
+        <div>
+            { loading ? 
+            <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+            </div>
+            :
+            <ItemList products = {products}/ >
+            }
+        </div> 
+    )
+}
+export default ItemDetailContainer;
